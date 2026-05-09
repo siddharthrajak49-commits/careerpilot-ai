fdb07f47b680635c6b499e982b23574885c71f2c
Siddharth Kumar <siddharthrajak49@gmail.com>
2026-05-09 09:41:32 +0530

    updated firebase config

--- careerpilot-ai/frontend/src/firebase.js
+++ careerpilot-ai/frontend/src/firebase.js
@@ -1,49 +1,29 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import {
-  getAuth,
-  GoogleAuthProvider
-} from "firebase/auth";
-
-/* =========================
-   FIREBASE CONFIG
-========================= */
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBI5gGTEhkk5FqCPbeASgRmF-ZYNu3qkXI",
   authDomain: "careerpilot-4b77a.firebaseapp.com",
   projectId: "careerpilot-4b77a",
-  storageBucket:
-    "careerpilot-4b77a.firebasestorage.app",
-  messagingSenderId:
-    "253121350618",
-  appId:
-    "1:253121350618:web:cba366138629387f158ee3",
-  measurementId:
-    "G-MPLY6JGHFB"
+  storageBucket: "careerpilot-4b77a.firebasestorage.app",
+  messagingSenderId: "253121350618",
+  appId: "1:253121350618:web:3059fb45dd619890158ee3",
+  measurementId: "G-EPYYQWT1MH",
 };
 
-/* =========================
-   INIT
-========================= */
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-const app =
-  initializeApp(
-    firebaseConfig
-  );
-
-/* =========================
-   AUTH
-========================= */
-
-export const auth =
-  getAuth(app);
-
-export const provider =
-  new GoogleAuthProvider();
+export const provider = new GoogleAuthProvider();
 
 /* Optional */
 
 provider.setCustomParameters({
-  prompt: "select_account"
+  prompt: "select_account",
 });
